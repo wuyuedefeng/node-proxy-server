7775a193fd5a6be5c71fd84a1522ff429e6673af
sen <578595193@qq.com>
2019-08-20 15:28:27 +0800

    FEAT: target origin support in url path

--- index.js
+++ index.js
@@ -21,25 +21,45 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
   proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
 });
 
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  // res.setHeader('content-type', 'application/json;charset=utf-8');
+  proxyRes.pipe(res)
+});
+
 //
 // Create your custom server and just call `proxy.web()` to proxy
 // a web request to the target passed in the options
 // also you can use `proxy.ws()` to proxy a websockets request
 //
 var server = http.createServer(function(req, res) {
-
-  let queryObj = qs.parse(url.parse(req.url).query)
+  let urlObj = url.parse(req.url)
+  let queryObj = qs.parse(urlObj.query)
   let cookies = new Cookies(req, res)
-  let target = req.headers['x-proxy-target'] || cookies.get('x-proxy-target') || queryObj['x-proxy-target']
+
+  let pathMatch = urlObj.path.match(/^\/http(s)?:\/\/(?:\w+\.)+\w+/i)
+
+  let target = ''
+  if (pathMatch) {
+    target = urlObj.path.replace(/^\//i, '')
+  } else {
+    let origin = `${req.headers['x-proxy-target'] || cookies.get('x-proxy-target') || queryObj['x-proxy-target'] || ''}`
+    target = `${origin}${urlObj.path}`
+  }
+
   if (!target) {
     res.write('please set header, cookie or query field [x-proxy-target] for your target host')
     res.end()
   } else {
     // You can define here your custom logic to handle the request
     // and then proxy the request.
-    proxy.web(req, res, { target, changeOrigin: true });
+    proxy.web(req, res, {
+      target,
+      changeOrigin: true,
+      autoRewrite: true,
+      selfHandleResponse : true
+    });
   }
 });
 
-console.log("listening on port 5050")
+console.log("listening on port 3099")
 server.listen(3099);
