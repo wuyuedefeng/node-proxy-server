8bf2ec78a56518cecde3d5f65063b5cf30c23ddc
sen <578595193@qq.com>
2019-08-20 17:09:14 +0800

    .

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ proxy.on('proxyRes', function (proxyRes, req, res) {
   // res.setHeader('content-type', 'application/json;charset=utf-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
   proxyRes.pipe(res)
 });
 
